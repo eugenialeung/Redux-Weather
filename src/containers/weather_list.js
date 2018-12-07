a9c03c9ceef068721c1ec3fcb0baab13dca5bb53
Eugenia Leung <eugenia_leung316@yahoo.com>
2018-12-06 16:24:35 -0800

    implement sparklines charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Chart from "../components/chart";
+import _ from "lodash";
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const name = cityData.city.name;
+    // temps is a good place to convert temperature units
+    const temps = cityData.list.map(weather => weather.main.temp);
+    // const temps = _.map(
+    //   cityData.list.map(weather => weather.main.temp),
+    //   temp => temp - 273
+    // );
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(humidity => humidity.main.pressure);
+    // console.log(temps);
     return (
-      <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" units="K" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="purple" units="%" />
+        </td>
       </tr>
     );
   }
@@ -16,9 +37,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
